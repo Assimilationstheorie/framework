e2bdb6798dd3cd007b2c9f3b9de1b982c9720301
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2019-12-16 17:15:13 +0100

    Update Inspiring.php
    Live long and in peace!

--- src/Illuminate/Foundation/Inspiring.php
+++ src/Illuminate/Foundation/Inspiring.php
@@ -44,6 +44,7 @@ class Inspiring
             'If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius',
             'No surplus words or unnecessary actions. - Marcus Aurelius',
             'People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius',
+            'Things are only impossible until they are not. - Jean-Luc Picard'
         ])->random();
     }
 }
